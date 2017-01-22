b9aba17742ca4516a636a9c8740cd01d3f10becc
Ushakov Andrey <std_string@mail.ru>
2017-01-22 23:37:14 +0500

    finished implementation of transport

--- CalcApp/src/Transport/TransportFactory.cpp
+++ CalcApp/src/Transport/TransportFactory.cpp
@@ -1,6 +1,11 @@
 #include <QObject>
 
+#include "LowLevel/TransportLowLevel.h"
+#include "IMessageCheckStrategy.h"
 #include "ITransport.h"
+#include "SimpleMessageCheckStrategy.h"
+#include "Transport.h"
+#include "TransportConfig.h"
 #include "TransportFactory.h"
 
 namespace CalcApp
@@ -12,6 +17,11 @@ TransportFactory::TransportFactory(QObject *parent) : QObject(parent)
 
 ITransport* TransportFactory::Create(QObject *parent)
 {
+    // TODO (std_string) : implement working with config in future
+    TransportConfig transportConfig(1, "127.0.0.1", 6666, 7777);
+    ITransport *transportLowLevel = new TransportLowLevel(transportConfig, parent);
+    IMessageCheckStrategy *messageCheckStrategy = new SimpleMessageCheckStrategy(transportConfig.MaxDelayedCount, parent);
+    return new Transport(transportLowLevel, messageCheckStrategy, parent);
 }
 
 }
