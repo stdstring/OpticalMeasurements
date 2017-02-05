d88fb46e81dc0d8a3edaabbe3c2193a71158b911
Ushakov Andrey <std_string@mail.ru>
2017-02-05 12:04:20 +0500

    added minimal deserialization (for extracting package and calculation numbers) into transport

--- CalcApp/src/Transport/MessageInfoFactory.cpp
+++ CalcApp/src/Transport/MessageInfoFactory.cpp
@@ -1,3 +1,6 @@
+#include <QDataStream>
+#include <QIODevice>
+
 #include "Message.h"
 #include "MessageInfo.h"
 #include "MessageInfoFactory.h"
@@ -7,8 +10,16 @@ namespace CalcApp
 
 MessageInfo CreateMessageInfo(Message const &message)
 {
-    // TODO (std_string) : we will make some minimal deserialization here
-    return MessageInfo(0, 0);
+    // TODO (std_string) : we will correct this minimal deserialization here
+    // Min deserialization:
+    // 1) quint32 with package number
+    // 2) quint32 with calculation number
+    // 3) other data
+    quint32 packageNumber, calcNumber;
+    QDataStream stream(message.GetData());
+    stream.setVersion(QDataStream::Qt_5_5);
+    stream >> packageNumber >> calcNumber;
+    return MessageInfo(packageNumber, calcNumber);
 }
 
 }
--- CalcApp/tests/TransportTests/MessageInfoFactoryTests.cpp
+++ CalcApp/tests/TransportTests/MessageInfoFactoryTests.cpp
@@ -1,5 +1,8 @@
 #include <QByteArray>
 
+//#include <initializer_list>
+#include <vector>
+
 #include "gtest/gtest.h"
 
 #include "EqualityOperators.h"
@@ -10,9 +13,17 @@
 namespace CalcApp
 {
 
+MessageInfo CreateMessageInfo(std::vector<char> const &data)
+{
+    return CreateMessageInfo(Message(MessageType::DATA, QByteArray(data.data(), data.size())));
+}
+
 TEST(MessageInfoFactoryTests, Create)
 {
-    ASSERT_EQ(MessageInfo(0, 0), CreateMessageInfo(Message(MessageType::DATA, QByteArray())));
+    ASSERT_EQ(MessageInfo(1, 0), CreateMessageInfo({0, 0, 0, 1, 0, 0, 0, 0, 1, 2}));
+    ASSERT_EQ(MessageInfo(4, 2), CreateMessageInfo({0, 0, 0, 4, 0, 0, 0, 2, 10, 11, 12, 13, 14}));
+    ASSERT_EQ(MessageInfo(11, 66), CreateMessageInfo({0, 0, 0, 11, 0, 0, 0, 66}));
+    ASSERT_EQ(MessageInfo(257, 256), CreateMessageInfo({0, 0, 1, 1, 0, 0, 1, 0, 33}));
 }
 
 }
