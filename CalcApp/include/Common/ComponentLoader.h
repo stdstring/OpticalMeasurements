72fba6b639b7cf8cc4c157976b03e6ee682e44ef
Ushakov Andrey <std_string@mail.ru>
2017-03-10 22:41:13 +0500

    added loader for loading components from dir

--- CalcApp/include/Common/ComponentLoader.h
+++ CalcApp/include/Common/ComponentLoader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <QDirIterator>
+#include <QList>
 #include <QObject>
 #include <QString>
 
@@ -11,7 +13,11 @@ class ComponentLoader
 public:
     template<typename TComponent> static TComponent* Load(QString const &location, QObject *parent)
     {
-        return qobject_cast<TComponent*>(ComponentLoader::Load(location, parent));
+        QObject *obj = Load(location, parent);
+        TComponent *component = qobject_cast<TComponent*>(obj);
+        if (component == nullptr && obj != nullptr)
+            delete obj;
+        return component;
     }
 
     static QObject* Load(QString const &location, QObject *parent);
@@ -21,6 +27,34 @@ public:
     ComponentLoader(ComponentLoader&&) = delete;
     ComponentLoader& operator=(ComponentLoader const&) = delete;
     ComponentLoader& operator=(ComponentLoader&&) = delete;
+};
+
+class ComponentsDirLoader
+{
+public:
+    template<typename TComponent> static QList<TComponent*> Load(QString const &directory, QObject *parent, bool processSubdirectories)
+    {
+        QDirIterator::IteratorFlag iteratorFlag = processSubdirectories ? QDirIterator::Subdirectories : QDirIterator::NoIteratorFlags;
+        // TODO (std_string) : think about name filters
+        QDirIterator iterator(directory, {"*.so", "*.dll"}, QDir::Files, iteratorFlag);
+        QList<TComponent*> dest;
+        while (iterator.hasNext())
+        {
+            iterator.next();
+            TComponent *component = ComponentLoader::Load<TComponent>(iterator.fileInfo().absoluteFilePath(), parent);
+            if (component != nullptr)
+                dest.append(component);
+        }
+        return dest;
+    }
+
+    static QList<QObject*> Load(QString const &directory, QObject *parent, bool processSubdirectories);
+
+    ComponentsDirLoader() = delete;
+    ComponentsDirLoader(ComponentsDirLoader const&) = delete;
+    ComponentsDirLoader(ComponentsDirLoader&&) = delete;
+    ComponentsDirLoader& operator=(ComponentsDirLoader const&) = delete;
+    ComponentsDirLoader& operator=(ComponentsDirLoader&&) = delete;
 };
 
 }
--- CalcApp/src/Common/ComponentLoader.cpp
+++ CalcApp/src/Common/ComponentLoader.cpp
@@ -1,3 +1,6 @@
+#include <QDir>
+#include <QDirIterator>
+#include <QList>
 #include <QObject>
 #include <QPluginLoader>
 #include <QString>
@@ -13,6 +16,22 @@ QObject* ComponentLoader::Load(QString const &location, QObject *parent)
     QObject *component = loader.instance();
     component->setParent(parent);
     return component;
+}
+
+QList<QObject*> ComponentsDirLoader::Load(QString const &directory, QObject *parent, bool processSubdirectories)
+{
+    QDirIterator::IteratorFlag iteratorFlag = processSubdirectories ? QDirIterator::Subdirectories : QDirIterator::NoIteratorFlags;
+    // TODO (std_string) : think about name filters
+    QDirIterator iterator(directory, {"*.so", "*.dll"}, QDir::Files, iteratorFlag);
+    QList<QObject*> dest;
+    while (iterator.hasNext())
+    {
+        iterator.next();
+        QObject *obj = ComponentLoader::Load(iterator.fileInfo().absoluteFilePath(), parent);
+        if (obj != nullptr)
+            dest.append(obj);
+    }
+    return dest;
 }
 
 }
