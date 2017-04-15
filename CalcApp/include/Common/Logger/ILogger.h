19f88efd56f968be627d0a54b8fd9ee5374463c1
Ushakov Andrey <std_string@mail.ru>
2017-04-15 13:54:32 +0500

    changed content of the ILogger

--- CalcApp/include/Common/Logger/ILogger.h
+++ CalcApp/include/Common/Logger/ILogger.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <QObject>
 #include <QString>
 
 namespace CalcApp
@@ -11,32 +12,43 @@ class LoggerCategory
 {
 };
 
-class ILogger
+class ILogger : public QObject
 {
 public:
-    LogLevel GetLogLevel() const;
-    void SetLogLevel(LogLevel level);
-    void WriteDebug(QString const &message);
-    void WriteDebug(LoggerCategory const &category, QString const &message);
-    void WriteDebugFormat(QString const &format, ...);
-    void WriteDebugFormat(LoggerCategory const &category, QString const &format, ...);
-    void WriteInfo(QString const &message);
-    void WriteInfo(LoggerCategory const &category, QString const &message);
-    void WriteInfoFormat(QString const &message, ...);
-    void WriteInfoFormat(LoggerCategory const &category, QString const &message, ...);
-    void WriteWarning(QString const &message);
-    void WriteWarning(LoggerCategory const &category, QString const &message);
-    void WriteWarningFormat(QString const &message, ...);
-    void WriteWarningFormat(LoggerCategory const &category, QString const &message, ...);
-    void WriteError(QString const &message);
-    void WriteError(LoggerCategory const &category, QString const &message);
-    void WriteErrorFormat(QString const &message, ...);
-    void WriteErrorFormat(LoggerCategory const &category, QString const &message, ...);
-    void WriteCritical(QString const &message);
-    void WriteCritical(LoggerCategory const &category, QString const &message);
-    void WriteCriticalFormat(QString const &message, ...);
-    void WriteCriticalFormat(LoggerCategory const &category, QString const &message, ...);
+    explicit ILogger(LogLevel level, QObject *parent = nullptr);
+    virtual ~ILogger() {}
+
+    LogLevel GetLogLevel() const { return _level; }
+    void SetLogLevel(LogLevel level) { _level = level; }
+    virtual void WriteDebug(QString const &message) = 0;
+    virtual void WriteDebug(LoggerCategory const &category, QString const &message) = 0;
+    virtual void WriteDebugFormat(QString const &format, ...) = 0;
+    virtual void WriteDebugFormat(LoggerCategory const &category, QString const &format, ...) = 0;
+    virtual void WriteInfo(QString const &message) = 0;
+    virtual void WriteInfo(LoggerCategory const &category, QString const &message) = 0;
+    virtual void WriteInfoFormat(QString const &message, ...) = 0;
+    virtual void WriteInfoFormat(LoggerCategory const &category, QString const &message, ...) = 0;
+    virtual void WriteWarning(QString const &message) = 0;
+    virtual void WriteWarning(LoggerCategory const &category, QString const &message) = 0;
+    virtual void WriteWarningFormat(QString const &message, ...) = 0;
+    virtual void WriteWarningFormat(LoggerCategory const &category, QString const &message, ...) = 0;
+    virtual void WriteError(QString const &message) = 0;
+    virtual void WriteError(LoggerCategory const &category, QString const &message) = 0;
+    virtual void WriteErrorFormat(QString const &message, ...) = 0;
+    virtual void WriteErrorFormat(LoggerCategory const &category, QString const &message, ...) = 0;
+    virtual void WriteCritical(QString const &message) = 0;
+    virtual void WriteCritical(LoggerCategory const &category, QString const &message) = 0;
+    virtual void WriteCriticalFormat(QString const &message, ...) = 0;
+    virtual void WriteCriticalFormat(LoggerCategory const &category, QString const &message, ...) = 0;
+private:
+    LogLevel _level;
 };
+
+ILogger::ILogger(LogLevel level, QObject *parent) :
+    QObject(parent),
+    _level(level)
+{
+}
 
 }
 
