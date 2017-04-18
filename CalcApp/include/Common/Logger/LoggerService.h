15c8252f106a6ab1c4a67642c4fbb48ec37a3f39
Ushakov Andrey <std_string@mail.ru>
2017-04-18 23:09:33 +0500

    created service for logger

---
+++ CalcApp/include/Common/Logger/LoggerService.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <QObject>
+#include <QString>
+#include <QThread>
+
+#include <memory>
+
+#include "ILogger.h"
+
+namespace CalcApp
+{
+
+class LoggerServiceWorker : public QObject
+{
+    Q_OBJECT
+public:
+    LoggerServiceWorker(std::shared_ptr<ILogger> logger);
+
+private:
+    std::shared_ptr<ILogger> _logger;
+
+signals:
+    void WriteDebug(const QString &message);
+    void WriteDebug(const LoggerCategory &category, const QString &message);
+    void WriteInfo(const QString &message);
+    void WriteInfo(const LoggerCategory &category, const QString &message);
+    void WriteWarning(const QString &message);
+    void WriteWarning(const LoggerCategory &category, const QString &message);
+    void WriteError(const QString &message);
+    void WriteError(const LoggerCategory &category, const QString &message);
+
+private slots:
+    void ProcessWriteDebug(const QString &message);
+    void ProcessWriteDebug(const LoggerCategory &category, const QString &message);
+    void ProcessWriteInfo(const QString &message);
+    void ProcessWriteInfo(const LoggerCategory &category, const QString &message);
+    void ProcessWriteWarning(const QString &message);
+    void ProcessWriteWarning(const LoggerCategory &category, const QString &message);
+    void ProcessWriteError(const QString &message);
+    void ProcessWriteError(const LoggerCategory &category, const QString &message);
+};
+
+class LoggerService : public ILogger
+{
+public:
+    LoggerService(std::shared_ptr<ILogger> logger);
+
+    virtual void WriteDebug(const QString &message) override;
+    virtual void WriteDebug(const LoggerCategory &category, const QString &message) override;
+    virtual void WriteInfo(const QString &message) override;
+    virtual void WriteInfo(const LoggerCategory &category, const QString &message) override;
+    virtual void WriteWarning(const QString &message) override;
+    virtual void WriteWarning(const LoggerCategory &category, const QString &message) override;
+    virtual void WriteError(const QString &message) override;
+    virtual void WriteError(const LoggerCategory &category, const QString &message) override;
+
+private:
+    std::shared_ptr<LoggerServiceWorker> _worker;
+    std::shared_ptr<QThread> _workerThread;
+};
+
+}
---
+++ CalcApp/src/Common/Logger/LoggerService.cpp
@@ -0,0 +1,122 @@
+#include <QObject>
+#include <QString>
+#include <QThread>
+
+#include <memory>
+
+#include "ILogger.h"
+#include "LoggerService.h"
+
+namespace CalcApp
+{
+
+namespace
+{
+
+typedef void (LoggerServiceWorker::*SimpleWriteFun) (QString const&);
+typedef void (LoggerServiceWorker::*CategoryWriteFun) (LoggerCategory const&, QString const&);
+
+}
+
+LoggerServiceWorker::LoggerServiceWorker(std::shared_ptr<ILogger> logger) : _logger(logger)
+{
+    QObject::connect(this, static_cast<SimpleWriteFun>(&LoggerServiceWorker::WriteDebug), this, static_cast<SimpleWriteFun>(&LoggerServiceWorker::ProcessWriteDebug));
+    QObject::connect(this, static_cast<CategoryWriteFun>(&LoggerServiceWorker::WriteDebug), this, static_cast<CategoryWriteFun>(&LoggerServiceWorker::ProcessWriteDebug));
+    QObject::connect(this, static_cast<SimpleWriteFun>(&LoggerServiceWorker::WriteInfo), this, static_cast<SimpleWriteFun>(&LoggerServiceWorker::ProcessWriteInfo));
+    QObject::connect(this, static_cast<CategoryWriteFun>(&LoggerServiceWorker::WriteInfo), this, static_cast<CategoryWriteFun>(&LoggerServiceWorker::ProcessWriteInfo));
+    QObject::connect(this, static_cast<SimpleWriteFun>(&LoggerServiceWorker::WriteWarning), this, static_cast<SimpleWriteFun>(&LoggerServiceWorker::ProcessWriteWarning));
+    QObject::connect(this, static_cast<CategoryWriteFun>(&LoggerServiceWorker::WriteWarning), this, static_cast<CategoryWriteFun>(&LoggerServiceWorker::ProcessWriteWarning));
+    QObject::connect(this, static_cast<SimpleWriteFun>(&LoggerServiceWorker::WriteError), this, static_cast<SimpleWriteFun>(&LoggerServiceWorker::ProcessWriteError));
+    QObject::connect(this, static_cast<CategoryWriteFun>(&LoggerServiceWorker::WriteError), this, static_cast<CategoryWriteFun>(&LoggerServiceWorker::ProcessWriteError));
+}
+
+void LoggerServiceWorker::ProcessWriteDebug(const QString &message)
+{
+    _logger.get()->WriteDebug(message);
+}
+
+void LoggerServiceWorker::ProcessWriteDebug(const LoggerCategory &category, const QString &message)
+{
+    _logger.get()->WriteDebug(category, message);
+}
+
+void LoggerServiceWorker::ProcessWriteInfo(const QString &message)
+{
+    _logger.get()->WriteInfo(message);
+}
+
+void LoggerServiceWorker::ProcessWriteInfo(const LoggerCategory &category, const QString &message)
+{
+    _logger.get()->WriteInfo(category, message);
+}
+
+void LoggerServiceWorker::ProcessWriteWarning(const QString &message)
+{
+    _logger.get()->WriteWarning(message);
+}
+
+void LoggerServiceWorker::ProcessWriteWarning(const LoggerCategory &category, const QString &message)
+{
+    _logger.get()->WriteWarning(category, message);
+}
+
+void LoggerServiceWorker::ProcessWriteError(const QString &message)
+{
+    _logger.get()->WriteError(message);
+}
+
+void LoggerServiceWorker::ProcessWriteError(const LoggerCategory &category, const QString &message)
+{
+    _logger.get()->WriteError(category, message);
+}
+
+LoggerService::LoggerService(std::shared_ptr<ILogger> logger) :
+    ILogger(LogLevel::DEBUG),
+    _worker(new LoggerServiceWorker(logger)),
+    _workerThread(new QThread())
+{
+    _worker.get()->moveToThread(_workerThread.get());
+    _workerThread.get()->start();
+}
+
+void LoggerService::WriteDebug(const QString &message)
+{
+    emit _worker.get()->WriteDebug(message);
+}
+
+void LoggerService::WriteDebug(const LoggerCategory &category, const QString &message)
+{
+    emit _worker.get()->WriteDebug(category, message);
+}
+
+void LoggerService::WriteInfo(const QString &message)
+{
+    emit _worker.get()->WriteInfo(message);
+}
+
+void LoggerService::WriteInfo(const LoggerCategory &category, const QString &message)
+{
+    emit _worker.get()->WriteInfo(category, message);
+}
+
+void LoggerService::WriteWarning(const QString &message)
+{
+    emit _worker.get()->WriteWarning(message);
+}
+
+void LoggerService::WriteWarning(const LoggerCategory &category, const QString &message)
+{
+    emit _worker.get()->WriteWarning(category, message);
+}
+
+void LoggerService::WriteError(const QString &message)
+{
+    emit _worker.get()->WriteError(message);
+}
+
+void LoggerService::WriteError(const LoggerCategory &category, const QString &message)
+{
+    emit _worker.get()->WriteError(category, message);
+}
+
+}
