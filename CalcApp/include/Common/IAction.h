e68912469a1d577323ef3deffa6008f078fd8cb0
Ushakov Andrey <std_string@mail.ru>
2017-05-13 16:39:53 +0500

    implementation of multithreading actions. intermediate commit

--- CalcApp/include/Common/IAction.h
+++ CalcApp/include/Common/IAction.h
@@ -15,18 +15,21 @@ class IAction : public QObject
 {
     Q_OBJECT
 public:
-    explicit IAction(std::shared_ptr<Context> context/*, QObject *parent = nullptr*/) : QObject(/*parent*/)
+    explicit IAction(std::shared_ptr<Context> context) : QObject(), _context(context)
     {
-        Q_UNUSED(context)
     }
 
     virtual QString GetName() = 0;
-    /*virtual void Run(Context &context) = 0;*/
-    /*virtual void StartAction(Context &context) = 0;*/
+    //virtual void Run(Context &context) = 0;
+    //virtual void StartAction(Context &context) = 0;
 
 protected:
     virtual void ProcessStartImpl() = 0;
     virtual void ProcessStopImpl() = 0;
+    std::shared_ptr<Context> GetContext() const { return _context; }
+
+private:
+    std::shared_ptr<Context> _context;
 
 signals:
     void DataCompleted(QString const &key);
@@ -44,15 +47,5 @@ public slots:
         ProcessStopImpl();
     }
 };
-
-/*void IAction::ProcessStart()
-{
-    ProcessStartImpl();
-}
-
-void IAction::ProcessStop()
-{
-    ProcessStopImpl();
-}*/
 
 }
