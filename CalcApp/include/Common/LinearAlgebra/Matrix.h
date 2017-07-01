0e585f41f624df695ab1599b0bf05307409e54dc
Ushakov Andrey <std_string@mail.ru>
2017-07-01 23:22:02 +0500

    creation of Linear Algebra adapter. intermediate commit

--- CalcApp/include/Common/LinearAlgebra/Matrix.h
+++ CalcApp/include/Common/LinearAlgebra/Matrix.h
@@ -1,10 +1,34 @@
 #pragma once
 
+#include <initializer_list>
+
 namespace CalcApp
 {
 
 class Matrix
 {
+public:
+    Matrix(unsigned int columnCount, unsigned int rowCount, double fillValue = 0);
+    explicit Matrix(std::initializer_list<double> const &data);
+    explicit Matrix(std::initializer_list<std::initializer_list<double>> const &data);
+    // TODO (std_string) : think about default/custom impl
+    explicit Matrix(Matrix const &other) = default;
+    explicit Matrix(Matrix &&other) = default;
+    Matrix& operator=(Matrix const &other) = default;
+    Matrix& operator=(Matrix &&other) = default;
+
+    unsigned int GetColumnCount() const;
+    unsigned int GetRowCount() const;
+    double GetValue(unsigned row, unsigned column) const;
+    void SetValue(unsigned row, unsigned column, double value);
 };
+
+Matrix operator+(Matrix const &left, Matrix const &right);
+Matrix operator-(Matrix const &left, Matrix const &right);
+Matrix operator*(Matrix const &left, Matrix const &right);
+
+Matrix& operator+=(Matrix& left, Matrix const& right);
+Matrix& operator-=(Matrix& left, Matrix const& right);
+Matrix& operator*=(Matrix& left, Matrix const& right);
 
 }
--- CalcApp/src/Common/LinearAlgebra/Matrix.cpp
+++ CalcApp/src/Common/LinearAlgebra/Matrix.cpp
@@ -1,5 +1,60 @@
+#include <initializer_list>
+
 #include "Matrix.h"
 
 namespace CalcApp
 {
+
+Matrix::Matrix(unsigned int columnCount, unsigned int rowCount, double fillValue)
+{
+}
+
+Matrix::Matrix(std::initializer_list<double> const &data)
+{
+}
+
+Matrix::Matrix(std::initializer_list<std::initializer_list<double>> const &data)
+{
+}
+
+unsigned int Matrix::GetColumnCount() const
+{
+}
+
+unsigned int Matrix::GetRowCount() const
+{
+}
+
+double Matrix::GetValue(unsigned row, unsigned column) const
+{
+}
+
+void Matrix::SetValue(unsigned row, unsigned column, double value)
+{
+}
+
+Matrix operator+(Matrix const &left, Matrix const &right)
+{
+}
+
+Matrix operator-(Matrix const &left, Matrix const &right)
+{
+}
+
+Matrix operator*(Matrix const &left, Matrix const &right)
+{
+}
+
+Matrix& operator+=(Matrix& left, Matrix const& right)
+{
+}
+
+Matrix& operator-=(Matrix& left, Matrix const& right)
+{
+}
+
+Matrix& operator*=(Matrix& left, Matrix const& right)
+{
+}
+
 }
