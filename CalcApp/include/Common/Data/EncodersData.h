02df7e77310fa09a3e9d20ebbc94171d9fc50211
Ushakov Andrey <std_string@mail.ru>
2017-07-08 20:48:36 +0500

    defined encoders

--- CalcApp/include/Common/Data/EncodersData.h
+++ CalcApp/include/Common/Data/EncodersData.h
@@ -5,6 +5,19 @@ namespace CalcApp
 
 struct EncodersData
 {
+public:
+    EncodersData();
+
+    int GlobalRotationX;
+    int GlobalTransferX;
+    int GlobalRotationY;
+    int GlobalTransferY;
+    int GlobalRotationZ;
+    int GlobalTransferZ;
+    int LocalRotation;
+    int LocalTransfer;
+    int SensorTransfer;
+    int Value;
 };
 
 }
--- CalcApp/src/Common/Data/EncodersData.cpp
+++ CalcApp/src/Common/Data/EncodersData.cpp
@@ -2,4 +2,19 @@
 
 namespace CalcApp
 {
+
+EncodersData::EncodersData() :
+    GlobalRotationX(0),
+    GlobalTransferX(0),
+    GlobalRotationY(0),
+    GlobalTransferY(0),
+    GlobalRotationZ(0),
+    GlobalTransferZ(0),
+    LocalRotation(0),
+    LocalTransfer(0),
+    SensorTransfer(0),
+    Value(0)
+{
+}
+
 }
