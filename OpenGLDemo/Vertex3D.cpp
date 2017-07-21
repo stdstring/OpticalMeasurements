d11409036d3264074c966882836a451921cf2f5c
Ushakov Andrey <std_string@mail.ru>
2017-07-21 23:30:12 +0500

    created Vertex3D

--- OpenGLDemo/Vertex3D.cpp
+++ OpenGLDemo/Vertex3D.cpp
@@ -3,9 +3,12 @@
 namespace OpenGLDemo
 {
 
-Vertex3D::Vertex3D()
+Vertex3D::Vertex3D() : X(0), Y(0), Z(0)
 {
+}
 
+Vertex3D::Vertex3D(double x, double y, double z) : X(x), Y(y), Z(z)
+{
 }
 
 }
--- OpenGLDemo/Vertex3D.h
+++ OpenGLDemo/Vertex3D.h
@@ -3,10 +3,13 @@
 namespace OpenGLDemo
 {
 
-class Vertex3D
+struct Vertex3D
 {
 public:
     Vertex3D();
+    Vertex3D(double x, double y, double z);
+
+    double X, Y, Z;
 };
 
 }
