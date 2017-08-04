a3e4c3bd38cb760721135397a0bd6a30763a0936
Ushakov Andrey <std_string@mail.ru>
2017-08-04 14:45:56 +0500

    added Vertex3DData structure

--- CalcApp/include/Common/Data/Vertex3D.h
+++ CalcApp/include/Common/Data/Vertex3D.h
@@ -14,4 +14,15 @@ public:
     double Z;
 };
 
+// TODO (std_string) : This set of encoders is specific for Stage2 only. Think about location of it
+struct Vertex3DData
+{
+public:
+    Vertex3DData();
+    Vertex3DData(Vertex3D const &surfacePoint, Vertex3D const &sensor);
+
+    Vertex3D SurfacePoint;
+    Vertex3D Sensor;
+};
+
 }
--- CalcApp/src/Common/Data/Vertex3D.cpp
+++ CalcApp/src/Common/Data/Vertex3D.cpp
@@ -11,4 +11,14 @@ Vertex3D::Vertex3D(double x, double y, double z) : X(x), Y(y), Z(z)
 {
 }
 
+Vertex3DData::Vertex3DData()
+{
+}
+
+Vertex3DData::Vertex3DData(Vertex3D const &surfacePoint, Vertex3D const &sensor) :
+    SurfacePoint(surfacePoint),
+    Sensor(sensor)
+{
+}
+
 }
