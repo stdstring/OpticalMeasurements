8eaf95243021a7337ef8381a19676372ea20f27d
Ushakov Andrey <std_string@mail.ru>
2017-08-08 23:08:53 +0500

    refactoring

--- CalcApp/src/MainApp/ResultProcessor.cpp
+++ CalcApp/src/MainApp/ResultProcessor.cpp
@@ -38,6 +38,28 @@ void ShowViewer(ResultDef const &result, ViewersConfig const &viewers, LoggerPtr
                       QString("%1 %2 --header=\"%3\" %4").arg(viewer.Filename).arg(viewer.Args).arg(result.Descriptor).arg(result.DataFilename));
 }
 
+void ProcessEmptyResults()
+{
+    QMessageBox resultInfo;
+    resultInfo.setText("This chain doesn't contain any defined results for view.");
+    resultInfo.exec();
+}
+
+void ProcessSeveralResults(ActionChainDefPtr chain, MainConfigPtr config, LoggerPtr logger)
+{
+    QStringList resultItemsList;
+    std::transform(chain.get()->Results.cbegin(),
+                   chain.get()->Results.cend(),
+                   std::back_inserter(resultItemsList),
+                   [](ResultDef const &result){ return result.Descriptor; });
+    ChooseResultDialog chooseResultDialog(resultItemsList);
+    if (QDialog::Accepted == chooseResultDialog.exec())
+    {
+        int selectedResultIndex = chooseResultDialog.GetChosenItemIndex();
+        ShowViewer(chain.get()->Results[selectedResultIndex], config.get()->Viewers, logger);
+    }
+}
+
 }
 
 ResultProcessor::ResultProcessor(MainConfigPtr config, LoggerPtr logger, QObject *parent) :
@@ -50,31 +72,12 @@ ResultProcessor::ResultProcessor(MainConfigPtr config, LoggerPtr logger, QObject
 void ResultProcessor::ShowResult(ActionChainDefPtr chain)
 {
     // TODO (std_string) : use more functional approach
-    // TODO (std_string) : separate on several methods
     if (chain.get()->Results.size() == 0)
-    {
-        QMessageBox resultInfo;
-        resultInfo.setText("This chain doesn't contain any defined results for view.");
-        resultInfo.exec();
-    }
+        ProcessEmptyResults();
     else if (chain.get()->Results.size() == 1)
-    {
         ShowViewer(chain.get()->Results[0], _config.get()->Viewers, _logger);
-    }
     else
-    {
-        QStringList resultItemsList;
-        std::transform(chain.get()->Results.cbegin(),
-                       chain.get()->Results.cend(),
-                       std::back_inserter(resultItemsList),
-                       [](ResultDef const &result){ return result.Descriptor; });
-        ChooseResultDialog chooseResultDialog(resultItemsList);
-        if (QDialog::Accepted == chooseResultDialog.exec())
-        {
-            int selectedResultIndex = chooseResultDialog.GetChosenItemIndex();
-            ShowViewer(chain.get()->Results.at(selectedResultIndex), _config.get()->Viewers, _logger);
-        }
-    }
+        ProcessSeveralResults(chain, _config, _logger);
 }
 
 }
